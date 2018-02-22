132d76896157f4bd9fc6a7eb4e919f0f53ba664f
Korune <rwxgood@126.com>
2018-02-22 20:38:08 +0800

    添加撰写按钮

--- KOWeiBo/MainClasses/KOMainTabBarController.swift
+++ KOWeiBo/MainClasses/KOMainTabBarController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class KOMainTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ 
         setupChildControllers()
+        setupComposeButton()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // @objc 允许这个函数在运行时通过 OC 的消息机制被调用
+    @objc fileprivate func composeStatus() {
+        NSLog("撰写微博")
+    }
+    
+    // MARK: -
+    /// 撰写微博按钮
+    fileprivate lazy var composeButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named:"tabbar_compose_button"), for: .normal)
+        button.setImage(UIImage(named:"tabbar_compose_icon_add"), for: .normal)
+        button.setImage(UIImage(named:"tabbar_compose_icon_add_highlighted"), for:.highlighted)
+        
+        return button
+    }()
 }
 
 extension KOMainTabBarController {
@@ -28,6 +45,7 @@ extension KOMainTabBarController {
         let infos = [
             ["clsName" : "KOHomeViewController", "title" : "首页", "imageName" : "tabbar_home"],
             ["clsName" : "KOMessageViewController", "title" : "消息", "imageName" : "tabbar_message_center"],
+            ["clsName" : "UIViewController"],
             ["clsName" : "KODiscoverViewController", "title" : "发现", "imageName" : "tabbar_discover"],
             ["clsName" : "KOProfileViewController", "title" : "我", "imageName" : "tabbar_profile"]
         ]
@@ -62,5 +80,15 @@ extension KOMainTabBarController {
         
         let nav = KOBaseNavigationController(rootViewController: vc)
         return nav
+    }
+    
+    fileprivate func setupComposeButton() {
+        tabBar.addSubview(composeButton)
+        
+        let count = childViewControllers.count
+        // 减1是为了，将按钮的宽度变大盖住容错点，防止穿帮
+        let w = tabBar.bounds.width / CGFloat(count) - 1
+        composeButton.frame = tabBar.bounds.insetBy(dx: w * 2, dy: 0) // 类似 CGRectInset 的使用，正数向内缩进，负数向外扩展
+        composeButton.addTarget(self, action: #selector(composeStatus), for: .touchUpInside)
     }
 }
