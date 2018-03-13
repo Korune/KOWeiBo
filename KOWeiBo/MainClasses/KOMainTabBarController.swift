a0b922f7e6933aed5ffd92a3b5f0f34e01c42fe6
Korune <rwxgood@126.com>
2018-03-13 22:03:39 +0800

    将要显示的 tabBarController 子控制器界面的数据，输出到 JSON

--- KOWeiBo/MainClasses/KOMainTabBarController.swift
+++ KOWeiBo/MainClasses/KOMainTabBarController.swift
@@ -60,6 +60,10 @@ extension KOMainTabBarController {
             ["clsName" : "KOProfileViewController", "title" : "我", "imageName" : "tabbar_profile",
              "visitorInfo": ["imageName": "visitordiscover_image_profile", "message": "登录后，你的微博、相册、个人资料会显示在这里，展示给别人"]]
         ]
+        
+        let data = try? JSONSerialization.data(withJSONObject: infos, options: [.prettyPrinted])
+        (data as NSData?)?.write(toFile: "/Users/korune/Desktop/main.json", atomically: true)
+        
         var vcs = [UIViewController]()
         for dict in infos {
             vcs.append(controller(dict: dict))
